1c44b8cbcb89287bcd995e36c1c5344d5080de0f
Gwak Jimin <zimin406@yonsei.ac.kr>
2023-10-03 01:38:13 +0900

    Display books on page when page is loaded

--- js/script.js
+++ js/script.js
@@ -1,1 +1,61 @@
+const bookList = [];
+
+function Book(title, author, publisher) {
+    this.title = title;
+    this.author = author;
+    this.publisher = publisher;
+    this.isRead = false;
+}
+
+const book1 = new Book("Strike of Celeste", "Fiona Maralyn", "CalmPages");
+bookList.push(book1);
+
+const bookTable = document.querySelector("table.books>tbody");
+
+window.addEventListener("load", (event) => {
+    for(book of bookList) {
+        const bookRow = document.createElement("tr");
+
+        const bookTitle = document.createElement("td");
+        bookTitle.classList.add("book-title");
+        bookTitle.textContent = book.title;
+        bookRow.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement("td");
+        bookAuthor.classList.add("book-author");
+        bookAuthor.textContent = book.author;
+        bookRow.appendChild(bookAuthor);
+
+        const bookPublisher = document.createElement("td");
+        bookPublisher.classList.add("book-publisher");
+        bookPublisher.textContent = book.publisher;
+        bookRow.appendChild(bookPublisher);
+
+        const bookIsRead = document.createElement("td");
+        bookIsRead.classList.add("book-is-read");
+        bookIsRead.textContent = book.isRead ? "Yes" : "No";
+        bookRow.appendChild(bookIsRead);
+
+        const markReadCell = document.createElement("td");
+        markReadCell.classList.add("mark-raed");
+        const markReadButton = document.createElement("button");
+        markReadButton.textContent = "Mark Read";
+        markReadCell.appendChild(markReadButton);
+        bookRow.appendChild(markReadCell);
+        
+        const removeBookCell = document.createElement("td");
+        removeBookCell.classList.add("remove-book")
+        const removeBookButton = document.createElement("button");
+        removeBookButton.textContent = "Remove Book";
+        removeBookButton.classList.add("remove-book-button");
+        removeBookCell.appendChild(removeBookButton);
+        bookRow.appendChild(removeBookCell);
+
+        bookTable.appendChild(bookRow);
+
+    }
+})
+
+
+
 
